48851cd5502f8c7e05a0b0385938deb8c95fe8ad
ihsandwi17 <nurihsan719@gmail.com>
2022-11-27 03:50:04 +1400

    menyelesaikan video 18 Belajar Laravel 8 | 18. Create Post Form

--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     protected $guarded = ['id'];
 
@@ -54,6 +56,15 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+    
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
     
 }
--- laravel/resources/views/dashboard/layouts/main.blade.php
+++ laravel/resources/views/dashboard/layouts/main.blade.php
@@ -9,8 +9,23 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-    <!-- Custom styles for this template -->
+     <!-- Custom styles for this template -->
     <link href="/css/dashboard.css" rel="stylesheet">
+
+
+   <!-- Trix Editor -->
+     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0-beta.0/dist/trix.css">
+     <script type="text/javascript" src="https://unpkg.com/trix@2.0.0-beta.0/dist/trix.umd.min.js"></script>
+
+     <style>
+         trix-toolbar [data-trix-button-group="file-tools"]{
+             display: none;
+         }
+     </style>
+  
+    {{-- <link rel="stylesheet" type="text/css" href="/css/trix.css">
+        <script type="text/javascript" src="/js/trix.js"></script> --}}
+   
   </head>
   <body>
 
