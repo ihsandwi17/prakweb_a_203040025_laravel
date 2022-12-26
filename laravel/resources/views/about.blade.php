eb11d65d6837ae9f25e1a6da98da92cfd9597271
ihsandwi17 <nurihsan719@gmail.com>
2022-12-26 20:32:46 +1400

    membuat halaman about

--- laravel/resources/views/about.blade.php
+++ laravel/resources/views/about.blade.php
@@ -1,11 +1,52 @@
- 
 @extends('layouts.main')
 
 @section('container')
 
-<h1>Halaman About</h1>
-<h3>{{ $name }}</h3>
-<p>{{ $email }}</p>
-<img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
+<section>
+  <div class="container" style="margin-top: 5%;">
+    <h1 style="text-align: center;">Tentang Kami</h1>
+    <hr>
+    <p style="text-align:center; font-size:20px;">STX Anime List adalah website yang menyangkut dengan anime. Website ini menampilkan nilai suatu anime, dan detail lainnya.</p>
+    <p style="text-align:center; font-size:20px;">Website ini dibangun pada tahun 2022. Website STX Anime List memberikan informasi terupdate dan teraktual seputar dunia anime.</p>
+    <br>
+  </div>
+</section>
 
-@endsection
+<section style="background-color: rgba(5, 0, 36, 1); text-align:center; padding-top: 1%; padding-bottom: 1%; color:white; background-image:url(/storage/footer/bg.jpg); width:100%;">
+  <div class="container">
+    <h1>Tujuan Kami</h1>
+    <hr>
+    <p style="text-align:center; font-size:20px;">Kami menciptakan STX Anime List karena kami tahu, banyak pecinta anime yang ingin mengetahui lebih jauh seputar anime.</p>
+    <p style="text-align:center; font-size:20px;">Kami memberikan </p>
+  </div>
+  <br>
+</section>
+
+<section style="text-align: center; padding-top: 1%; padding-bottom: 1%;">
+  <div class="container">
+    <h1>Tim Kami</h1>
+    <hr>
+    <div class="row align-items-center">
+      <div class="col">
+        <img src="/storage/footer/profil-mail.jpg" alt="" style="border: 0px solid black; border-radius: 360px; max-width: 200px; margin: auto; margin-bottom:10px;">
+        <h3>Ismail Fikri</h3>
+        <p>Leader of Syntax Error</p>
+      </div>
+      <div class="col">
+        <img src="/storage/footer/profil-fikri.jpg" alt="" style="border: 0px solid black; border-radius: 360px; max-width: 200px; margin: auto; margin-bottom:10px;">
+        <h3>Fikri Ridwan F.</h3>
+        <p>Back-End of Syntax Error</p>
+      </div>
+      <div class="col">
+        <img src="/storage/footer/profil-ihsan.jpg" alt="" style="border: 0px solid black; border-radius: 360px; max-width: 200px; margin: auto; margin-bottom:10px;">
+        <h3>Nur Ihsan Dwi L.</h3>
+        <p>Front-End of Syntax Error</p>
+      </div>
+      <div class="col">
+        <img src="/storage/footer/profil-salsa.jpg" alt="" style="border: 0px solid black; border-radius: 360px; max-width: 200px; margin: auto; margin-bottom:10px;">
+        <h3>Salsabila Firdaus</h3>
+        <p>Front-End of Syntax Error</p>
+      </div>
+    </div>
+  </div>
+</section>
